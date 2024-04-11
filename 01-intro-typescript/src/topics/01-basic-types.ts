314426551844e472de9aa9fb9e9008e13f2f938c
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-04-11 10:49:09 -0600

    Introduccion a Typescript

---
+++ 01-intro-typescript/src/topics/01-basic-types.ts
@@ -0,0 +1,8 @@
+//Si las variables no cambian, usar tipado de const.
+
+const name = 'Zeyan'; // TypeScript infiere los tipados de manera automatica
+
+//  name = 123; // Los errores con TypeScript son mostrados en tiempo de escritura
+
+
+export{};
