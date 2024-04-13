2343142f5554c835ab60233ae1d8572d6235c94b
DarkZeyan <esonic0234@hotmail.com>
2024-04-13 16:26:40 -0600

    Finalizada la leccion de desestructuracion de arreglos

--- 01-intro-typescript/src/topics/05-basic-destructuring.ts
+++ 01-intro-typescript/src/topics/05-basic-destructuring.ts
@@ -24,10 +24,24 @@ const audioPlayer: AudioPlayer = {
 //     author,
 //     year
 // }} = audioPlayer;
-const {song:cancion, songDuration:duration, details} = audioPlayer;
-const {author, year} = details;
-console.log('Song: ', cancion);
-console.log('Duration: ', duration);
-console.log('Author: ', author);
-console.log('Year: ', year);
+// const {song:cancion, songDuration:duration, details} = audioPlayer;
+// const {author, year} = details;
+// console.log('Song: ', cancion);
+// console.log('Duration: ', duration);
+// console.log('Author: ', author);
+// console.log('Year: ', year);
+
+/* 
+    Desestructuracion de arreglos
+*/
+
+// const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+// const [, , trunks = 'No encontrado']: string[] = ['Goku', 'Vegeta', 'Trunks'];
+const [, , trunks = 'No encontrado']: string[] = ['Goku', 'Vegeta'];
+// const trunks = dbz[3];
+// console.log('Personaje 3: ', trunks || 'Personaje no encontrado');
+console.log('Personaje 3: ', trunks);
+
+
+
 export { };
