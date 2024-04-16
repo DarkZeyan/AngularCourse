43a755848f59b3e8a12e4082467e09580eef8ff1
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-04-16 11:31:50 -0600

    Composicion sobre herencia

--- 01-intro-typescript/src/topics/08-classes.ts
+++ 01-intro-typescript/src/topics/08-classes.ts
@@ -21,18 +21,41 @@ export class Person {
 
 // Extends viene de la herencia de clases
 // Hay que priorizar la composición sobre la herencia
-export class Hero extends Person{
+// export class Hero extends Person{
+//     constructor(
+//         public alterEgo:string,
+//         public age: number,
+//         public realName: string,
+//     ){
+//         super(realName, 'New York'); // Constructor de la clase padre.
+
+//     }
+// }
+
+// Composicion
+export class Hero{
+
+    // public person: Person;
+
     constructor(
         public alterEgo:string,
         public age: number,
         public realName: string,
+        public person:Person
     ){
-        super(realName, 'New York'); // Constructor de la clase padre.
+        // this.person = new Person(realName) // Constructor de la clase padre.
+        //Con esto podremos observar que la clase padre ahora aparece como una
+        // propiedad dentro de nuestra clase
 
+        // Sin embargo, se presenta el problema de la dependencia directa dentro de nuestro
+        // nuevo constructor
+
+        // Para evitar este problema se solicita en el cosntructor la inyección de dependencias
     }
 }
 
+const peter = new Person('Peter Parker', 'New York');
 
-const spiderman = new Hero('Spiderman',45, 'Peter Parker');
+const spiderman = new Hero('Spiderman',45, 'Peter Parker',peter);
 
 console.log(spiderman);
