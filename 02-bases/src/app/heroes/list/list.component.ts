90d9b16ab5dd09647aa27e5a0790d08b0c992254
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-05-02 11:18:41 -0600

    Leccion de ngTemplate y ngIfElse

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
   public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
