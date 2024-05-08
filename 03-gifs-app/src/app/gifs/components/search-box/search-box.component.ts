327c17df6c8a574908969dde0af2a0b18a89071c
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-05-08 11:33:13 -0600

    Directiva ViewChild

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -9,9 +9,31 @@ import { Component } from '@angular/core';
     id=""
     class="form-control"
     placeholder="Buscar gifs..."
+    (keyup.enter)="searchTag()"
+    #txtTagInput
   >
   `
 })
 export class SearchBoxComponent {
 
+  // Se puede utilizar el keyup.enter para detectar cuando se presiona la tecla enter
+  // es un evento propio de angular para detectarlo y funciona
+  // como una alternativa a la directiva @ViewChild
+
+/*
+  ViewChild y ViewChildren son directivas que enlazan el resultado
+  final de la vista del componente baado en el predicado a la propiedad de la clase del componente.
+  No aplica a directivas estructurales como *ngIf o *ngFor.
+
+  Syntax:
+  @ViewChild( predicate, options ) myChildComponen;
+  @ViewChildren( predicate, options ) myChildComponents;
+*/
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value
+    console.log({newTag});
+  }
 }
