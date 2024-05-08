d7c5d78493163b58daa88b3dcafeff0aed14562a
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-05-08 13:08:55 -0600

    Realizar peticiones HTTP

--- 03-gifs-app/src/app/app.module.ts
+++ 03-gifs-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Se importa el módulo HttpClientModule para hacer peticiones HTTP
 
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule, // Se añade el módulo HttpClientModule
     GifsModule,
     SharedModule
   ],
--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 
 // Cuando se trabaja con el provideIn root
@@ -13,9 +14,9 @@ export class GifsService {
 
   private _tagsHistory: string[] = [];
   private _apiKey: string = 'c6gCUjBKI0JrnfVk0JSLGqjfldFbsh3c';
+  private _serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory]; // Como los arreglos en javascript se pasan como referencia, se hace una copia del arreglo para que no se modifique el original
@@ -32,8 +33,32 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0,10); // Se limita a 10 elementos
   }
 
-  searchTag(tag: string):void {
+  searchTag(tag: string): void{
     if(tag.length === 0) return; // Validacion de que si se envia nada, no se haga nada
     this.organizedHistory(tag);
+    // Manera con el fetch API de javascript
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=c6gCUjBKI0JrnfVk0JSLGqjfldFbsh3c&q=valorant&limit=10')
+    //   .then( resp => resp.json() )
+    //   .then( data => console.log(data))
+
+    // Otro modo de javascript
+    // const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=c6gCUjBKI0JrnfVk0JSLGqjfldFbsh3c&q=valorant&limit=10');
+    // const data = await resp.json();
+    // console.log(data);
+
+    // Este http es un observable, no es una promesa
+    // El observable es un objeto que a lo largo del tiempo puede emitir valores difernetes
+    // y se puede subscribir a el para recibir esos valores que emite a lo largo de su vida
+
+    // Podemos utilizar el objeto HttpParams para enviar los parametros de la URL de manera mas ordenada
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)
+      .set('limit','10')
+      .set('q', tag);
+
+    this.http.get(`${this._serviceUrl}/search?`,{params})
+      .subscribe( (resp: any) => {
+        console.log(resp);
+      });
   }
 }
